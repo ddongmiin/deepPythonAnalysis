e2fec9e77805fdf10492f798aef91489cb183da3
giblesdeepmind <ehdals1323@gmail.com>
2021-04-01 20:51:48 +0900

    py4e regular expression

--- text/py4e_regular_expression.py
+++ text/py4e_regular_expression.py
@@ -1,3 +1,7 @@
+"""
+edwith의 py4e 정규표현식 강좌를 참조했습니다.
+"""
+
 import re
 
 hand = open('mbox-short.txt')
@@ -61,3 +65,92 @@ extracted by using parentheses
 x = "From tjdrud1323@naver.com sat jan 5"
 y = re.findall("\S+@\S+", x)
 print(y)
+
+"""
+Fine-Truning Strin Extraction
+Parentheses are not part of the match - but they tell where to start and stop what string to extract
+"""
+
+# 괄호는 추출 시작범위와 끝 부분을 정해준다. 즉, 괄호안의 부분만 추출해라 
+x = "From tjdrud1323@naver.com sat jan 5"
+y = re.findall("From (\S+@\S+", x)
+print(y)
+
+#using find
+data = "From stephen.maruard@uct.ac.za Sat Jan  5 09:14:16 2008"
+atpos = data.find('@')
+print(atpos)
+sppos = data.find(' ', atpos) #find의 두 번째 파라미터는 파라미터의 시작 위치
+print(sppos)
+host = data[atpos+1 : sppos]
+print(host)
+
+"""
+The double split pattern
+Sometimes we split a line one way, and then grab one of the pieces of the line and split that piece again
+"""
+
+#using split, split의 디폴트 인자는 공백
+words = data.split()
+email = words[1].split('@')
+print(email[1])
+
+"""
+The Reg Version
+__ ^공백 -> 공백 말고 다 찾아라
+"""
+
+lin = 'From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008'
+y = re.findall('@([^ ]*)', lin)
+print(y)
+
+#1. @를 찾아라
+#2. 괄호안의 것들을 찾을 건데
+#3. 우선, ^ 공백이 아냐
+#4. 걔네들을 무한히 반복해
+
+"""
+Even cooler Regex Version
+데이터 클리닝까지 동시에
+"""
+
+lin = 'From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008'
+y = re.findall('^From .*@([^ ]*)', lin)
+print(y)
+
+#1. 라인의 첫 번째 줄을 살펴봐
+#2. From이 있는지 살펴봐
+#3. 공백 한칸 띄우고 모든 문자/숫자 반복
+#4. @를 찾아봐
+#5. 괄호안의 것을 찾아봐
+
+"""
+Spam Confidence
+"""
+
+hand = open('mbox-short.txt')
+numlist = list()
+for line in hand :
+    line = line.rstrip()
+    stuff = re.findall('^X-DSPAM-Confidence: ([0-9.]+)', line)
+    if len(stuff) != 1 : continue
+    num = float(stuff[0])
+    numlist.append(num)
+print('Maximum: ', max(numlist))
+
+#1. 조건에 안 맞는 애들 X-DSPAM으로 시작하지 않는 애들은 Skip 
+#2. 그리고 동시에 continue를 통해 조건에 맞지 않는 애들은 아래 코드를 실행하지 않음
+
+"""
+Escape character
+If you want a special regular expression character to just behave normally 
+(most of the time) you prefix it with \
+"""
+
+x = 'We just received $10.00 for cookies.'
+y = re.findall('\$[0-9.]+', x)
+print(y)
+
+#1. 역슬래쉬 후 $를 찾아라
+#2. 0~9 숫자 그리고, .을 찾아라
+#3. 걔네만 무한 반복
